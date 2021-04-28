d584ef1ebf15879fe58529247a652e65146f1855
CollyneJumah <collynejumah2010@gmail.com>
2021-04-28 14:01:33 +0300

    reverted bugg on

--- src/Create.js
+++ src/Create.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import history, { useHistory } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 
 const Create = () => {
     const [title, setTitle]= useState('')
